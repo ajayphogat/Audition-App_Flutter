abec713d0a34fea2aebe1e5b5f043456a094e800
rajaFlutter <raja.dey@iskylar.in>
2022-08-08 18:37:43 +0530

    small changes done

--- lib/pages/paymentPage/paymentPage.dart
+++ lib/pages/paymentPage/paymentPage.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/bottomNavigation/bottomNavigationBar.dart';
 import 'package:first_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -16,7 +17,6 @@ class _PaymentPageState extends State<PaymentPage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _textController = TextEditingController();
   }
@@ -25,7 +25,6 @@ class _PaymentPageState extends State<PaymentPage> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    print(screenHeight * 0.005);
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
@@ -71,27 +70,45 @@ class _PaymentPageState extends State<PaymentPage> {
                         ),
                         title: Container(
                           width: screenWidth * 0.50,
-                          height: screenHeight * 0.15,
+                          height: screenHeight * 0.30,
                           alignment: Alignment.center,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(15),
                           ),
                           child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text("Payment Successfull"),
-                              Lottie.asset(
-                                "asset/lottie/successfully_done.json",
-                                width: screenWidth * 0.20,
-                                height: screenWidth * 0.20,
-                              ),
-                              Container(
-                                width: screenWidth * 0.30,
-                                height: screenHeight * 0.08,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: secondoryColor,
+                              SizedBox(
+                                width: screenWidth * 0.40,
+                                height: screenWidth * 0.40,
+                                child: Lottie.asset(
+                                  "asset/lottie/successfully_done.json",
+                                  fit: BoxFit.contain,
                                 ),
-                                child: const Text("GO HOME"),
+                              ),
+                              const Text("Payment Successfull"),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.pushNamedAndRemoveUntil(
+                                      context,
+                                      BottomNavigationPage.routeName,
+                                      (route) => false);
+                                },
+                                child: Container(
+                                  width: screenWidth * 0.38,
+                                  height: screenHeight * 0.047,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: secondoryColor,
+                                  ),
+                                  child: const Text(
+                                    "GO HOME",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ],
                           ),
@@ -102,8 +119,8 @@ class _PaymentPageState extends State<PaymentPage> {
                 },
                 child: Container(
                   alignment: Alignment.center,
-                  width: 150,
-                  height: 40,
+                  width: screenWidth * 0.38,
+                  height: screenHeight * 0.047,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8),
                     color: const Color(0xFFF9D422),
