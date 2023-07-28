ac3bb337da6fa6f4001d993f46780d58aaef8e46
krishanjangid <krishanjangid516@gmail.com>
2023-07-28 12:31:35 +0530

    getAllStudio api config

--- auth/audition_user_auth.js
+++ auth/audition_user_auth.js
@@ -1385,7 +1385,7 @@ userAuth.get("/api/showWorkingJobs", auth, async (req, res) => {
 //show all studios data in audition
 userAuth.get("/api/audition/getAllStudio", auth, async (req, res) => {
   try {
-    const studios = await studioModel.find({ status: false });
+    const studios = await studioModel.find({ status: true });
     let newResult = [];
     studios.forEach((studio) => {
       newResult.push(studio.toObject());
